fa1789745de8a66d8f5a5ea4bc1755bde2abdb71
simondanielsson <simon.danielsson99@hotmail.com>
2024-09-22 18:53:17 +0200

    Fix api url

--- src/lib/utils/api.js
+++ src/lib/utils/api.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.API_URL;
+export const API_URL = process.env.API_URL || 'http://approved-api-620648088594.europe-north1.run.internal/api/v1';
 
 export async function loginUser(formData) {
 	try {
